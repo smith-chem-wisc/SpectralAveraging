3d80d007d32a681da9e844978196e74e0bf61d14
nbollis <nbollis@comcast.net>
2022-10-31 13:46:40 -0500

    Added some test coverage

--- Tests/TestWeighting.cs
+++ Tests/TestWeighting.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 using SpectralAveraging;
 namespace Tests
 {
