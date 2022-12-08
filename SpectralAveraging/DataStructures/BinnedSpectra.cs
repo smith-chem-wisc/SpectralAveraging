61aee57b3756809afe918a27ce7a75021caf0acb
avcarr2 <avcarr2@wisc.edu>
2022-12-08 13:25:57 -0600

    Pre-modification of consume spectra method

---
+++ SpectralAveraging/DataStructures/BinnedSpectra.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nett;
+using SpectralAveraging.NoiseEstimates;
+
+namespace SpectralAveraging.DataStructures
+{
+
+    public class BinnedSpectra
+    {
+        public List<PixelStack> PixelStacks { get; set; }
+        public Dictionary<int, double> NoiseEstimates { get; private set; }
+        public Dictionary<int, double> ScaleEstimates { get; private set; }
+        public Dictionary<int, double> Weights { get; private set; }
+
+        public BinnedSpectra()
+        {
+            PixelStacks = new List<PixelStack>();
+            NoiseEstimates = new Dictionary<int, double>();
+            ScaleEstimates = new Dictionary<int, double>();
+            Weights = new Dictionary<int, double>();
+        }
+
+        public void ProcessPixelStacks(SpectralAveragingOptions options)
+        {
+            foreach (var pixelStack in PixelStacks)
+            {
+                pixelStack.PerformRejection(options);
+            }
+        }
+
+        public void ConsumeSpectra(double[][] xArrays, double[][] yArrays, 
+            double[] totalIonCurrents, int numSpectra, double binSize)
+        {
+            double min = 100000;
+            double max = 0;
+            for (int i = 0; i < numSpectra; i++)
+            {
+                min = Math.Min(xArrays[i][0], min);
+                max = Math.Max(xArrays[i].Max(), max);
+            }
+
+            int numberOfBins = (int)Math.Ceiling((max - min) * (1 / binSize));
+
+            double[][] xValuesBin = new double[numberOfBins][];
+            double[][] yValuesBin = new double[numberOfBins][];
+            // go through each scan and place each (m/z, int) from the spectra into a jagged array
+
+            // 1) find all values of x that fall within a bin.
+            
+            // 2) perform a cubic spline interpolation to find the value of the 
+            // central point (aka the bin center). 
+
+            for (int i = 0; i < numSpectra; i++)
+            {
+                // currently this updates the y value with the most recent value from the array. 
+                // what it really needs to do is a linear interpolation. 
+
+                var binIndices = xArrays[i]
+                    .Select(i => (int)Math.Floor((i - min) / binSize))
+                    .ToArray();
+                for (int j = 0; j < xArrays[i].Length; j++)
+                {
+                     
+                    int binIndex = (int)Math.Floor((xArrays[i][j] - min) / binSize);
+                    if (xValuesBin[binIndex] == null)
+                    {
+                        xValuesBin[binIndex] = new double[numSpectra];
+                        yValuesBin[binIndex] = new double[numSpectra];
+                    }
+
+                    xValuesBin[binIndex][i] = xArrays[i][j];
+                    yValuesBin[binIndex][i] = yArrays[i][j];
+                }
+            }
+
+            xValuesBin = xValuesBin.Where(p => p != null).ToArray();
+            yValuesBin = yValuesBin.Where(p => p != null).ToArray();
+
+            double[] xArray = new double[xValuesBin.Length];
+
+
+            for (var i = 0; i < xArray.Length; i++)
+            {
+                xArray[i] = xValuesBin[i].Where(p => p != 0).Average();
+                double x = xArray[i];
+                var pixelStack = new PixelStack(x);
+                pixelStack.AddIntensityVals(yValuesBin[i]);
+                PixelStacks.Add(pixelStack);
+            }
+        }
+
+        public void PerformNormalization(double[][] yArrays, double[] tics)
+        {
+            for (int i = 0; i < yArrays.Length; i++)
+            {
+                SpectrumNormalization.NormalizeSpectrumToTic(yArrays[i],
+                    tics[i], tics.Average());
+            }
+        }
+
+        public void CalculateNoiseEstimates(double[][] yArrays)
+        {
+            for (var i = 0; i < yArrays.Length; i++)
+            {
+                double[] yArray = yArrays[i];
+                bool success = MRSNoiseEstimator.MRSNoiseEstimation(yArray, 0.01, out double noiseEstimate);
+                // if the MRS noise estimate fails to converge, go by regular standard deviation
+                if (!success)
+                {
+                    noiseEstimate = BasicStatistics.CalculateStandardDeviation(yArray);
+                }
+
+                NoiseEstimates.Add(i, noiseEstimate);
+            }
+        }
+
+        public void CalculateScaleEstimates()
+        {
+            // insert avgdev method code here
+        }
+
+        public void CalculateWeights()
+        {
+            foreach (var entry in NoiseEstimates)
+            {
+                var successScale = ScaleEstimates.TryGetValue(entry.Key,
+                    out double scale);
+                if (!successScale) continue;
+
+                var successNoise = NoiseEstimates.TryGetValue(entry.Key,
+                    out double noise);
+                if (!successNoise) continue;
+
+                double weight = 1d / Math.Pow((scale * noise), 2);
+
+                Weights.Add(entry.Key, weight);
+            }
+        }
+
+        public void MergeSpectra()
+        {
+            double[] weights = Weights.OrderBy(i => i.Key)
+                .Select(i => i.Value)
+                .ToArray();
+            foreach (var pixelStack in PixelStacks)
+            {
+                pixelStack.Average(weights);
+            }
+        }
+
+        public double[][] GetMergedSpectrum()
+        {
+            double[] xArray = PixelStacks.Select(i => i.Mz).ToArray();
+            double[] yArray = PixelStacks.Select(i => i.MergedValue).ToArray();
+            return new[] { xArray, yArray };
+        }
+    }
+}
---
+++ Tests/TestBinnedSpectra.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO.MzML;
+using MassSpectrometry;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using NUnit.Framework.Interfaces;
+using SpectralAveraging;
+using SpectralAveraging.DataStructures;
+
+namespace Tests;
+
+public class TestBinnedSpectra
+{
+    [Test]
+    public void TestConsumeSpectra()
+    {
+        BinnedSpectra binnedSpectra = new();
+
+        double[][] xarrays = new[]
+        {
+            new double[] { 0, 1, 2, 3, 4 },
+            new double[] { 0, 1, 2, 3, 4 },
+            new double[] { 0.1, 1.1, 2.1, 3.1, 4.1}
+        };
+        double[][] yarrays = new[]
+        {
+            new double[] { 10, 11, 12, 13, 14 },
+            new double[] { 11, 12, 13, 14, 15 },
+            new double[] { 20, 25, 30, 35, 40 }
+        };
+        double[] tics = new double[3];
+        tics[0] = yarrays[0].Sum();
+        tics[1] = yarrays[1].Sum();
+        tics[2] = yarrays[2].Sum();
+
+        int numSpectra = 3;
+        double binSize = 1.0;
+
+        binnedSpectra.ConsumeSpectra(xarrays, yarrays, tics, numSpectra, binSize); 
+        Assert.True(binnedSpectra.PixelStacks.Count == xarrays[0].Length);
+        Assert.True(binnedSpectra.PixelStacks[0].Mz == 0.1d);
+        Assert.That(binnedSpectra.PixelStacks[2].Intensity, 
+            Is.EqualTo(new double[] {12, 13, 30}));
+    }
+    [Test]
+    public void TestConsumeSpectraUnequalArrayLength()
+    {
+        BinnedSpectra binnedSpectra = new();
+
+        double[][] xarrays = new[]
+        {
+            new double[] { 0, 1, 2, 3, 3.5, 4 },
+            new double[] { 0, 1, 2, 3, 4 },
+            new double[] { 0.1, 1.1, 2.1, 3.1, 4.1}
+        };
+        double[][] yarrays = new[]
+        {
+            new double[] { 10, 11, 12, 12, 13, 14 },
+            new double[] { 11, 12, 13, 14, 15 },
+            new double[] { 20, 25, 30, 35, 40 }
+        };
+        double[] tics = new double[3];
+        tics[0] = yarrays[0].Sum();
+        tics[1] = yarrays[1].Sum();
+        tics[2] = yarrays[2].Sum();
+
+        int numSpectra = 3;
+        double binSize = 1.0;
+
+        binnedSpectra.ConsumeSpectra(xarrays, yarrays, tics, numSpectra, binSize);
+        Assert.True(binnedSpectra.PixelStacks.Count == xarrays[0].Length);
+        Assert.True(binnedSpectra.PixelStacks[0].Mz == 0.1d);
+        Assert.That(binnedSpectra.PixelStacks[2].Intensity,
+            Is.EqualTo(new double[] { 12, 13, 30 }));
+    }
+}
